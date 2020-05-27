c70f68daa47a5016693b0663e991d79839cb5151
Monit Galav <51854843+mongalav@users.noreply.github.com>
2020-05-27 12:38:55 +0530

    counting the duplicate elements
    duplicate numbers in a sorted arrays

---
+++ counting_and_finding_duplicate_elements.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespacec std;
+
+int main(){
+    int a[] = {3,6,8,8,10,12,15,15,15,20};
+    int i = 0;
+    for(;i<n-1;i++){
+        if(a[i] == a[i+1]){
+            int j=i+1;
+            while(a[i]==a[j])j++;
+            printf("%d is occuring %d times",a[i],j-i);
+            i = j-1;
+        }
+    }
